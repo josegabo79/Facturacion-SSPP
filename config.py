8b286d8c561a8e59c720957904338bbd9ef92bfa
josegabo79 <134791837+josegabo79@users.noreply.github.com>
2026-04-30 16:07:10 -0500

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 archivos_procesados_memoria = set()
 
 # --- RUTAS DE ARCHIVOS ---
-ruta_excel = "https://pactia-my.sharepoint.com/:x:/p/jblandon/IQBByiYCKLQpT4JUOqcorATHAS7CasuVrB70SFxOeH4amVc?download=1"
+ruta_excel = "https://pactia-my.sharepoint.com/:x:/p/jblandon/IQBByiYCKLQpT4JUOqcorATHAQypG55Iqy2rQzidfCA32zU?e=bWd6Af"
 
 carpetas_edificios = {
     "Buró 51": r"C:\Users\JoseGabrielBlandonHe\OneDrive - Pactia\01 JEFATURA PY\2. Energia\Proyecto SSPP\Facturas\Buro 51",
